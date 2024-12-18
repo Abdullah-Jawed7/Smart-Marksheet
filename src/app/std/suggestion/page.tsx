6e7a55afbe65e5f04eb000abb3ae575263d7ad62
Abdullah-Jawed7 <oneandonly0o0.hhh7@gmail.com>
2024-12-18 21:17:44 +0500

    suggestion

---
+++ src/app/std/suggestion/page.tsx
@@ -0,0 +1,35 @@
+"use client"
+
+import Gemini from "@/components/compo/gemini"
+import { useStudentContext } from "@/components/hook/data"
+import { Card, CardContent, CardHeader} from "@/components/ui/card"
+import { BrainCircuit } from "lucide-react";
+
+export default function Suggestions() {
+    const { studentData } = useStudentContext();
+const prompt = `
+Analyze the student's performance: Name: ${studentData.name}, Class: ${studentData.class}.
+- Based on the analysis, identify weeknesses and provide suggestion for improvement in weeknesses.
+- Provide a plan for improvement for the student to achieve better performance.
+Here's the data: ${JSON.stringify(studentData.subjects)}.
+ Note: Please avoid Asteriks and provide only 5 suggestion and their one line introduction.
+`;
+
+    return (
+      <div className="p-6">
+        <Card>
+            <CardHeader> 
+                <div className="flex items-center gap-2">
+            <BrainCircuit className="h-5 w-5" />
+            <h2 className="text-xl font-semibold">Improvement Suggestions</h2>
+          </div> 
+          </CardHeader>
+            <CardContent>
+                {<Gemini instruction={prompt} />}
+            </CardContent>
+        </Card>
+        <h2 className="text-2xl font-bold"></h2>
+      </div>
+    )
+  }
+  
